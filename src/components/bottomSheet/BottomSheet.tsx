9c25f976446dec02465e7b608ba12f9f654d4062
Irfan <sikumbangirfann@gmail.com>
2023-08-02 18:54:34 +0800

    fix: remove opacicty style from  and add  folder

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -1160,8 +1160,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
     //#region styles
     const containerAnimatedStyle = useAnimatedStyle(
       () => ({
-        opacity:
-          Platform.OS === 'android' && animatedIndex.value === -1 ? 0 : 1,
+        // opacity: Platform.OS === 'android' && animatedIndex.value === -1 ? 0 : 1,
         transform: [
           {
             translateY: animatedPosition.value,
